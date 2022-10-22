f948645db9033d140e6789338940f6d2fde3650f
Daniel <110158094+danielglencer@users.noreply.github.com>
2022-10-22 17:54:16 +0300

    first commit

---
+++ project/scheduler.py
@@ -0,0 +1,25 @@
+import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+import tzlocal
+from project.models import Book, Log
+from project import db, app
+
+# a method which runs both on startup and at 12am, ensures the overdue logs are updated as such
+def check_if_overdue():
+    with app.app_context():
+        arr = db.session.query(Log, Book).join(Book).filter(Log.overdue == False, Log.return_date == None)
+        today = datetime.datetime.today()
+        for log, book in arr:
+            borrow_time = today - log.borrow_date
+            # print(borrow_time.days)
+            if (borrow_time.days > 10 and book.type == 1) or (borrow_time.days > 5 and book.type == 2) or (borrow_time.days > 2 and book.type == 3):
+                print('here')
+                log.overdue = True
+        db.session.commit()
+        # return scheduler.print_jobs()
+
+#scheduling the job to run at 12am
+scheduler = BackgroundScheduler(timezone=str(tzlocal.get_localzone()))
+scheduler.add_job(func=check_if_overdue, trigger='cron', hour=0, minute=0)
+scheduler.start()
+
